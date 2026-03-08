cfffe0117e7f301fe8938db01bf3b9ba7f417ec3
Lerian <kanglerian@gmail.com>
2026-03-08 16:13:26 +0700

    offline

--- sw.js
+++ sw.js
@@ -1,28 +1,8 @@
-const cacheName = 'dzikir-v4.3'; // <--- NAIKKAN VERSI SETIAP ADA PERUBAHAN (v1 ke v2, dst)
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const cacheName = 'dzikir-v1';
+const assets = ['./', './index.html', './manifest.json'];
 
-// Tahap Install: Simpan file ke cache
 self.addEventListener('install', e => {
-  self.skipWaiting(); // Memaksa SW baru langsung aktif tanpa menunggu tab ditutup
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
-});
-
-// Tahap Activate: HAPUS CACHE LAMA
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName)
-            .map(key => caches.delete(key)) // Menghapus v1 saat v2 masuk
-      );
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
